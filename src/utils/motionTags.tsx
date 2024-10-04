7ab95a3070ce4e9288171035a644bf47586f5dd5
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-10-05 03:39:36 +0530

    add(motiontags:list,image,button,div)

---
+++ src/utils/motionTags.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+
+export const MotionImage = motion(Image)
+export const MotionDiv = motion.div
+export const MotionList = motion.li
+export const MotionButton = motion(Button)
