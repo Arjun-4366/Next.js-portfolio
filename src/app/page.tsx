9fbe7113844e3a82e394170c8415f07d77398825
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-10-20 05:08:41 +0530

    add download cv feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,16 @@ const luck = Luckiest_Guy({
 
 function page() {
   return (
-    <div className="h-dvh flex overflow-hidden md:flex-row flex-col items-center justify-between pt-5 w-full px-3">
-      {/* parent div starts*/}
+    <div className="h-dvh flex overflow-hidden md:flex-row flex-col items-center justify-between pt-5 w-full px-5">
+  
       <div className="flex flex-col h-full justify-center space-y-6 md:items-end items-center relative max-w-xl md:ml-auto flex-1 gap-5 mt-8">
         <MotionDiv
           initial="initial"
           animate="animate"
           variants={variants.rise}
           transition={transitions.rise}
-          className="absolute flex items-center gap-x-4 -rotate-90 md:-left-4 -left-28 md:top-auto top-auto text-white">
+          className="absolute flex items-center -rotate-90 md:-left-4 -left-28 md:top-auto top-auto text-white px-4 gap-4"
+        >
           <MotionDiv
             initial={{ scale: 0.8 }}
             animate={{ scale: 1.1 }}
@@ -35,11 +36,12 @@ function page() {
               duration: 1,
               repeatType: "reverse",
             }}
-            className="border-4 size-14 rounded-full grid place-items-center">
+            className="border-4 size-14 rounded-full grid place-items-center"
+          >
             <div className="size-3 rounded-full bg-white"></div>
           </MotionDiv>
-          <h4 className={cn("text-2xl", luck.className)}>
-            <span className="text-primary">MERN</span>
+          <h4 className={cn("text-2xl ", luck.className)}>
+            <span className="text-primary">Full Stack</span>
             <br />
             <span>Developer</span>
           </h4>
@@ -51,21 +53,29 @@ function page() {
           transition={transitions.moveRight}
           animate={"animate"}
           className={cn(
-            "md:text-right text-center  md:-mt-8 mx-8",
+            "md:text-right text-center md:-mt-8 mx-8",
             luck.className
           )}
-          style={{ top: "-20px" }}>
-          <h1 className="text-primary">
+          style={{ top: "-20px" }}
+        >
+          <h1 className="text-primary px-3">
             Arjun T V <br />
             <MotionButton
-              className="font-mono flex items-center justify-center mt-2 ml-3"
+              className="font-mono flex items-center justify-center mt-2 ml-3 hover:bg-accent"
               whileHover={{ scale: 0.95 }}
               initial="initial"
               animate="animate"
               variants={variants.moveUp}
-              transition={transitions.moveUp}>
-              <DownloadIcon className="mr-2" />
-              Download CV
+              transition={transitions.moveUp}
+            >
+              <a
+                href={`/Arjun_TV_Resume.pdf`}
+                className="flex justify-center"
+                download
+              >
+                <DownloadIcon className="mr-2" />
+                Download CV
+              </a>
             </MotionButton>
             <MotionDiv
               className="flex items-center justify-evenly gap-x-3 md:absolute mx-8 mt-3"
@@ -75,7 +85,8 @@ function page() {
               transition={{
                 ...transitions.moveDown,
                 delay: 1,
-              }}>
+              }}
+            >
               {SOCIAL_MEDIA.map((item, i) => (
                 <Link href={item.link}>
                   <Image
@@ -92,20 +103,20 @@ function page() {
           </h1>
         </MotionDiv>
       </div>
-      {/* parent div ends */}
-      <div className=" flex md:max-w-[50%] max-w-lg  items-end self-end">
+
+      <div className="flex max-w-xs md:max-w-sm items-end self-end">
         <MotionImage
           src={"/her01.png"}
           alt="hero"
           width={480}
           height={480}
-          className="rounded-full object-cover max-h-full mb-3"
+          className="rounded-full object-cover flex items-center justify-center max-h-full mb-3 md:w-[450px] w-[400px] sm:w-[400px]"
           initial="initial"
           animate="animate"
           variants={variants.moveLeft}
           transition={{
             ...transitions.moveLeft,
-            delay:1.5
+            delay: 1.5,
           }}
         />
       </div>
