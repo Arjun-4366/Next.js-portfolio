8b900907fa399b5da3006b9bd435865eca1b3741
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-10-21 19:07:53 +0530

    conversion from jsx to tsx

---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useToast } from "@/hooks/use-toast"
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+import React from "react";
+
+
+
+interface ToastType {
+  id:string,
+  title?:string,
+  description?:string,
+  action?:React.ReactElement;
+  [Key:string]:any
+}
+
+const Toaster:React.FC = ()=> {
+  const { toasts } = useToast()
+
+  return (
+    (<ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }:ToastType) {
+        return (
+          (<Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action }
+            <ToastClose altText=""/>
+          </Toast>)
+        );
+      })}
+      <ToastViewport />
+    </ToastProvider>)
+  );
+} 
+
+export default Toaster
