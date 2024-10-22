645120c37c2bf26a408d20503f29b1d50de783ea
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-10-23 00:00:52 +0530

    dynamically imported framer motion

--- src/utils/motionTags.tsx
+++ src/utils/motionTags.tsx
@@ -1,13 +1,13 @@
 
 import { Button } from "@/components/ui/button";
 import { Tabs } from "@/components/ui/tabs";
-
-import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
 
-export const MotionImage = motion(Image)
-export const MotionDiv = motion.div
-export const MotionList = motion.li
-export const MotionButton = motion(Button)
-export const MotionTabs = motion(Tabs)
+
+export const MotionImage = dynamic(()=>import('framer-motion').then(mod=>mod.motion(Image)),{ssr:false})
+export const MotionDiv = dynamic(()=>import('framer-motion').then(mod=>mod.motion.div),{ssr:false})
+export const MotionList = dynamic(()=>import('framer-motion').then(mod=>mod.motion.li),{ssr:false})
+export const MotionButton = dynamic(()=>import('framer-motion').then(mod=>mod.motion(Button)),{ssr:false})
+export const MotionTabs = dynamic(()=>import('framer-motion').then(mod=>mod.motion(Tabs)),{ssr:false})
