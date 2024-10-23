e33cd39bf779b4a46cecb729a4f1c1651f5e4814
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-10-23 15:23:20 +0530

    changed drawer position and styles

--- src/components/Containers/MobileMenuSection.tsx
+++ src/components/Containers/MobileMenuSection.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Button } from "../ui/button";
 import { ROUTES } from "@/utils/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { IoMenu } from "react-icons/io5";
-import { IoClose } from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
+import React, { useState } from "react";
 
 interface Route {
   id: number;
@@ -17,47 +16,50 @@ interface Route {
 const MobileMenuSection: React.FC = () => {
   const path = usePathname();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
   const handleMobileMenu = (): void => {
     setMenuOpen((prevMenuOpen) => !prevMenuOpen);
   };
+
   return (
-   
-<Drawer open={menuOpen} onOpenChange={setMenuOpen} >
+    <Drawer open={menuOpen} onOpenChange={setMenuOpen}>
       {menuOpen && (
         <div className="bg-black bg-opacity-60 z-40 fixed inset-0"></div>
       )}
-      <DrawerContent className="">
-        <div className="max-w-sm mx-auto w-full">
-          <div className="p-4 border-b space-y-6 list-none">
-  
-              {ROUTES.map((route: Route) => (
-                <li key={route.id}>
-                  <Link
-                    href={route.path}
-                    className={cn(
-                      "hover:text-primary",
-                      path === route.path && "text-primary"
-                    )} onClick={handleMobileMenu}>
-                    {route.name}
-                  </Link>
-                </li>
-              ))}
-           
+      <DrawerContent
+        className={cn(
+          "fixed bottom-0 left-0 right-0", 
+          "w-full rounded-lg flex flex-col items-center justify-center",
+          "transition-transform duration-300",
+          menuOpen ? "translate-y-0" : "translate-y-full", 
+          "bg-blue-500" 
+        )}
+      >
+        <div className="max-w-sm mx-auto w-full flex items-center">
+          <div className="p-4 border-b space-y-8 list-none ">
+            {ROUTES.map((route: Route) => (
+              <li key={route.id}>
+                <Link
+                  href={route.path}
+                  className={cn(
+                    "hover:text-primary",
+                    path === route.path && "text-primary"
+                  )}
+                  onClick={handleMobileMenu}
+                >
+                  {route.name}
+                </Link>
+              </li>
+            ))}
           </div>
         </div>
       </DrawerContent>
       <DrawerTrigger asChild className="md:hidden">
         <Button variant="outline" onClick={handleMobileMenu}>
-          {menuOpen ? (
-            <IoClose className="text-5xl" />
-          ) : (
-            <IoMenu className="text-5xl" />
-          )}
+          {menuOpen ? <IoClose className="text-5xl" /> : <IoMenu className="text-5xl" />}
         </Button>
       </DrawerTrigger>
     </Drawer>
-
-    
   );
 };
 
